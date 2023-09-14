f6cfefe330277f96c0fa65184603a6626dbc6501
davletova <davletova.a.g@gmail.com>
2023-09-14 17:59:33 +0600

    Написан класс TabBarController для создания начального контроллера со списком трекеров

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -22,27 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let onboarding = OnboardingViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
             window?.rootViewController = onboarding
         } else {
-            let tabBar = UITabBarController()
-
-            let lineView = UIView(frame: CGRect(x: 0, y: 0, width:tabBar.tabBar.frame.size.width, height: 1))
-            lineView.backgroundColor = UIColor.getAppColors(.tabBarBorder)
-            tabBar.tabBar.addSubview(lineView)
-
-            let navigationController = UINavigationController(rootViewController: TrackerCollectionView())
-            navigationController.tabBarItem = UITabBarItem(
-                title: NSLocalizedString("trackers", comment: "кнопка перехода на список трекеров"),
-                image: UIImage(named: "record.circle.fill"),
-                tag: 0
-            )
-
-            let statisticsViewController = StatisticsViewController()
-            statisticsViewController.tabBarItem = UITabBarItem(
-                title: NSLocalizedString("statistic", comment: "кнопка перехода на статистику"),
-                image: UIImage(named: "hare.fill"),
-                tag: 1
-            )
-            tabBar.setViewControllers([navigationController, statisticsViewController], animated: true)
-
+            let tabBar = TabBarViewController()
             window?.rootViewController = tabBar
         }
         
--- Tracker/TabBarViewController/TabBarViewController.swift
+++ Tracker/TabBarViewController/TabBarViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 final class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
-            let tabBar = UITabBarController()
-
-            let lineView = UIView(frame: CGRect(x: 0, y: 0, width:tabBar.tabBar.frame.size.width, height: 1))
-            lineView.backgroundColor = UIColor.getAppColors(.tabBarBorder)
-            tabBar.tabBar.addSubview(lineView)
-
-            let navigationController = UINavigationController(rootViewController: TrackerCollectionView())
-            navigationController.tabBarItem = UITabBarItem(
-                title: NSLocalizedString("trackers", comment: "кнопка перехода на список трекеров"),
-                image: UIImage(named: "record.circle.fill"),
-                tag: 0
-            )
-
-            let statisticsViewController = StatisticsViewController()
-            statisticsViewController.tabBarItem = UITabBarItem(
-                title: NSLocalizedString("statistic", comment: "кнопка перехода на статистику"),
-                image: UIImage(named: "hare.fill"),
-                tag: 1
-            )
-            tabBar.setViewControllers([navigationController, statisticsViewController], animated: true)
-        }
+        let lineView = UIView(frame: CGRect(x: 0, y: 0, width: self.tabBar.frame.size.width, height: 1))
+        lineView.backgroundColor = UIColor.getAppColors(.tabBarBorder)
+        
+        self.tabBar.addSubview(lineView)
+        
+        let navigationController = UINavigationController(rootViewController: TrackerCollectionView())
+        navigationController.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("trackers", comment: "кнопка перехода на список трекеров"),
+            image: UIImage(named: "record.circle.fill"),
+            tag: 0
+        )
+        
+        let statisticsViewController = StatisticsViewController()
+        statisticsViewController.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("statistic", comment: "кнопка перехода на статистику"),
+            image: UIImage(named: "hare.fill"),
+            tag: 1
+        )
+        
+        self.setViewControllers([navigationController, statisticsViewController], animated: true)
+    }
 }
